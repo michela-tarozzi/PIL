13ba2e45602e4965d94f8f105073a11d13fa11ff
m.tarozzi <m.tarozzi@nsi.it>
2017-10-04 21:17:35 +0200

    mes

---
+++ Client/src/main/java/main.java
@@ -0,0 +1,11 @@
+/**
+ * Created by m.tarozzi on 04/10/2017.
+ */
+public class main {
+
+    public static void main(String[] args) {launch(args);}
+
+    private static void launch(String[] args) {
+    }
+
+}
